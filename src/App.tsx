7c52177e32097114cc8dbd56d07a038600fb9b91
SamuMontoya <samu7montoya@gmail.com>
2024-08-06 16:15:24 -0500

    setup: add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <p>Gary Postres Frontend</p>
+      <div className="flex w-full h-[100vh] bg-[#D91B5E] justify-center items-center">
+        <p className="font-extrabold text-4xl text-[#F9B13D]">
+          Gary Postres Frontend
+        </p>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
