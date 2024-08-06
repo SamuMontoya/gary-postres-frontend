8d1e76e2731f02a25d337189b4863ae903289e19
SamuMontoya <samu7montoya@gmail.com>
2024-08-06 16:42:53 -0500

    feat: add router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { RoutesElements } from './routes'
 
 function App() {
   return (
-    <>
-      <div className="flex w-full h-[100vh] bg-[#D91B5E] justify-center items-center">
-        <p className="font-extrabold text-4xl text-[#F9B13D]">
-          Gary Postres Frontend
-        </p>
-      </div>
-    </>
+    <BrowserRouter>
+      <RoutesElements />
+    </BrowserRouter>
   )
 }
 
